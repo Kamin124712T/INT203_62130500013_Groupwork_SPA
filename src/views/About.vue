d9da7d59f1bd61c63a4479b970fbd142314b0e61
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-04-15 08:32:33 +0700

    [ADD] 62130500013 About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    
+  <div>
+    <div class="w-full h-16 mt-5 text-4xl text-red-800 font-medium">All images in this work are from this original website.</div>
+    <iframe src="https://www.ghibli.jp/works/" class="h-screen w-full"></iframe>
   </div>
 </template>
